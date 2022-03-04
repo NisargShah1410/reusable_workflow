2d0d54e434e5687e3d60a2b8e57ee9ce568d4ea1
NisargShah1410 <nisargshah01410@gmail.com>
2022-03-04 06:26:47 +0000

    first

---
+++ MySampleWebApp/Calculator.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySampleWebApp
+{
+    public class Calculator
+    {
+        public const string password = "SomeString";
+        public int Add(int a, int b)
+        {
+            if (a==b)
+            {
+
+            }
+            else
+            {
+
+            }
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return b - a;
+        }
+    }
+}
